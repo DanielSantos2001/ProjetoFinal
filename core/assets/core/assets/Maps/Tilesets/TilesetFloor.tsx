69852d500491b356b6edf2ac27a2e405a4d47b27
EmanuelLourencoLEI <96886657+EmanuelLourencoLEI@users.noreply.github.com>
2022-03-17 11:08:57 +0000

    Delete core/assets/core directory

--- core/assets/core/assets/Maps/Tilesets/TilesetFloor.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="TilesetFloor" tilewidth="8" tileheight="8" tilecount="2288" columns="44">
- <image source="NinjaAdventure/Backgrounds/Tilesets/TilesetFloor.png" width="352" height="417"/>
-</tileset>
--- core/assets/core/assets/Maps/Tilesets/TilesetHouse.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="TilesetHouse" tilewidth="8" tileheight="8" tilecount="2668" columns="58">
- <image source="NinjaAdventure/Backgrounds/Tilesets/TilesetHouse.png" width="464" height="368"/>
-</tileset>
