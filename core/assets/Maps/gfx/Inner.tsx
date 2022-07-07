0f9a406662725b3be0413ff6c3aca69a3b2c4547
EmanuelLourencoLEI <96886657+EmanuelLourencoLEI@users.noreply.github.com>
2022-07-07 18:16:23 +0100

    Add files via upload

---
+++ core/assets/Maps/gfx/Inner.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Inner" tilewidth="8" tileheight="8" tilecount="4000" columns="80">
+ <image source="Inner.png" width="640" height="400"/>
+ <tile id="1601">
+  <properties>
+   <property name="tpHouseDown" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1602">
+  <properties>
+   <property name="tpHouseUp" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1681">
+  <properties>
+   <property name="blocked" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1682">
+  <properties>
+   <property name="tpCastleUp" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1762">
+  <properties>
+   <property name="tpCastleDown" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1841">
+  <properties>
+   <property name="tpForestDown" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1842">
+  <properties>
+   <property name="tpForestUp" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
