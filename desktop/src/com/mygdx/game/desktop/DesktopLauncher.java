aae0b22b9b35e4d4acc950207444f161d68b39f6
EmanuelLourencoLEI <96886657+EmanuelLourencoLEI@users.noreply.github.com>
2022-07-15 12:26:29 +0100

    Add files via upload

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,6 +1,5 @@
 package com.mygdx.game.desktop;
 
-
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.KnightsOath;
@@ -8,6 +7,7 @@ import com.mygdx.game.KnightsOath;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = true;
 		new LwjglApplication(new KnightsOath(), config);
 	}
 }
